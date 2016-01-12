8dbcfc37d19ed8d05b3fecaf9f29c129a7bf52f0
Nick Clucas <nclucas01@gmail.com>
2016-01-12 16:39:12 -0500

    Avoid n+1 issues

--- app/controllers/api/jobs_controller.rb
+++ app/controllers/api/jobs_controller.rb
@@ -1,14 +1,14 @@
 class Api::JobsController < Api::BaseController
   def index
     if params[:job_name].present?
-      @job = Job.find_by(name: params[:job_name].downcase)
+      @job = Job.includes(:employees).find_by(name: params[:job_name].downcase)
       if @job.nil?
         render json: {error:"Job #{params[:job_name]} not found!"}
       else
         render json: @job
       end
     else
-      @jobs = Job.first(10)
+      @jobs = Job.includes(:employees).first(10)
       render json: @jobs
     end
   end
